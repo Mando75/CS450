147b1ad443f34906fcfc72f0d449ac42a0d27bb2
Bryan Müller <starwarsgeek0975@gmail.com>
2019-01-16 12:03:38 -0700

    workin kdtree

--- kNN/kdTree.py
+++ kNN/kdTree.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import heapq
 
 class kdNode:
     def __init__(self, point, left, right, depth):
@@ -12,20 +12,62 @@ class kdNode:
         tab = "\t" * self.depth
         return f"depth: {self.depth} point{self.point}\n{tab} left: {self.left} \n{tab} right: {self.right}"
 
+class kdNeighbor:
+    def __init__(self, node, distance, height):
+        self.node = node
+        self.distance = distance
+        self.height = height
+
+    def __lt__(self, other):
+        return self.distance < other.distance
+
+    def __le__(self, other):
+        return self.distance <= other.distance
+
+    def __gt__(self, other):
+        return self.distance > other.distance
+
+    def __ge__(self, other):
+        return self.distance >= other.distance
+
+    def __eq__(self, other):
+        return self.distance == other.distance
+
+class kdPrioritySet(object):
+    def __init__(self):
+        self.heap = []
+        self.set = set()
+
+    def add(self, neighbor):
+        # Use a tuple so we can track the unique sets
+        if tuple(neighbor.node[0]) not in self.set:
+            heapq.heappush(self.heap, neighbor)
+            self.set.add(tuple(neighbor.node[0]))
+
+    def pop(self):
+        neighbor = heapq.heappop(self.heap)
+        self.set.remove(tuple(neighbor.node[0]))
+
+    def clear(self):
+        self.heap = []
+        self.set = set()
+
+
 
 class kdTree:
     def __init__(self, points):
         self.tree = self.build_tree(points)
+        self.heap = kdPrioritySet()
 
     def build_tree(self, points, depth=0):
         try:
-            k = len(points[0])
+            k = len(points[0][0])
         except IndexError as e:
             return None
 
         axis = depth % k
 
-        sorted_points = sorted(points, key=lambda point: point[axis])
+        sorted_points = sorted(points, key=lambda point: point[0][axis])
         median = len(sorted_points) // 2
 
         return kdNode(sorted_points[median],
@@ -40,16 +82,16 @@ class kdTree:
     def return_nearest(self, root, point, depth=0):
 
         if root.right is None:
-            distance = np.sum((root.point - point)**2)
+            distance = np.sum((root.point[0] - point)**2)
             return root.point, distance, 0
         elif root.left is None:
-            distance = np.sum((root.point - point)**2)
+            distance = np.sum((root.point[0] - point)**2)
             return root.point, distance, 0
         else:
 
             axis = np.mod(depth, np.shape(point)[0])
 
-            if point[axis] < root.point[axis]:
+            if point[axis] < root.point[0][axis]:
                 best, distance, height = self.return_nearest(
                     root.left, point, depth + 1)
             else:
@@ -57,19 +99,24 @@ class kdTree:
                     root.right, point, depth + 1)
 
         if height <= 2:
-            if point[axis] < root.point[axis]:
+            if point[axis] < root.point[0][axis]:
                 best2, distance2, height2 = self.return_nearest(
                     root.right, point, depth + 1)
             else:
                 best2, distance2, height2 = self.return_nearest(
                     root.left, point, depth + 1)
 
-        distance3 = np.sum((root.point - point)**2)
+        distance3 = np.sum((root.point[0] - point)**2)
         if distance3 < distance2:
             distance2 = distance3
             best2 = root.point
         if distance2 < distance:
             distance = distance2
             best = best2
+        else:
+            neighbor = kdNeighbor(best2, distance, height + 1)
+            self.heap.add(neighbor)
 
+        neighbor = kdNeighbor(best, distance, height + 1)
+        self.heap.add(neighbor)
         return best, distance, height + 1
--- kNN/main.py
+++ kNN/main.py
@@ -1,5 +1,7 @@
 from common.ClassifierTesterIris import ClassifierTesterIris
 from kNN.kNNClassifier import kNNClassifier
+from kNN.kdTree import kdTree
+import numpy as np
 import argparse
 
 
@@ -26,8 +28,16 @@ def processArgs():
 
 
 def main():
-    args = processArgs()
-    test_knn(args.numTests, args.verbose)
+    # args = processArgs()
+    # test_knn(args.numTests, args.verbose)
+    test_data = np.array(
+        [[0, 1, 2], [5, 3, 9], [9, 0, 4], [4, 2, 7], [8, 6, 2], [3, 6, 1],
+         [6, 8, 2], [1, 6, 3], [1, 5, 9], [1, 3, 7], [6, 8, 3], [5, 7, 2],
+         [1, 3, 2], [1, 2, 7], [7, 4, 6], [9, 3, 1]])
+
+    tuple_list = [(point, i) for i, point in enumerate(test_data)]
+    k_tree = kdTree(tuple_list)
+    k_tree.return_nearest(k_tree.tree, [4, 5, 1])
 
 
 if __name__ == '__main__':
