02ec12735c23b79506ee7b0409db720501eb5b52
Bryan Müller <starwarsgeek0975@gmail.com>
2019-02-04 23:46:54 -0700

    rework my butt off

---
+++ DecisionTree/dTree2.py
@@ -0,0 +1,120 @@
+import pandas as pd
+import numpy as np
+
+
+class dNode:
+    def __init__(self, label, leaf=False):
+        self.label, self.leaf = label, leaf
+        self.children = {}
+
+    def append_child(self, child):
+        if self.leaf:
+            self.children = child
+        else:
+            self.children[child[0]] = child[1]
+        return self.children[child[0]]
+
+    def __repr__(self):
+        if self.leaf:
+            return self.label
+        else:
+            return f"{self.label} --> {self.children}"
+
+class dTree:
+    def __init__(self, data, labels, default_class=None):
+        self.root = self.makeTree(data, "class")
+        self.labels, self.default_class = labels, default_class
+
+    def __repr__(self):
+        return str(self.root)
+
+    def makeTree(self, data, class_name):
+        size = data[class_name].size
+        split_options = data[class_name].unique()
+        # Base Case 1:
+        # We don't have any more ways to split the data
+        if len(split_options) == 1:
+            return dNode(split_options[0], leaf=True)
+        # Base Case 2:
+        if size == 1:
+            return data[class_name].value_counts().index[0]
+
+        # Recursive tree construction
+        entropies = []
+        for column in data.columns:
+            if column is not class_name:
+                average_entropy = 0
+                for option in data[column].unique():
+                    q_set = data[data[column] == option][class_name]
+                    entropy = self.get_entropy(q_set)
+                    average_entropy += q_set.size / size * entropy
+                entropies.append([average_entropy, column])
+            if len(entropies) is 0:
+                return None
+            else:
+                entropies.sort()
+                best_split = entropies[0][1]
+
+            node = dNode(best_split)
+            for option in data[best_split].unique():
+                split_data = data[data[best_split] == option].drop(
+                    best_split, axis=1)
+                node.append_child(
+                    [option, self.makeTree(split_data, class_name)])
+            return node
+
+    def get_entropy(self, q_set):
+        probabilities = self.get_series_probabilities(q_set)
+        sum = 0
+        for p in probabilities:
+            if p is not 0:
+                sum -= p * np.log2(p)
+        return sum
+        # return reduce(lambda acc, p: acc - (p * np.log2(p)) if p != 0 else 0,
+        #               probabilities)
+
+    def get_series_probabilities(self, q_set):
+        counts = q_set.value_counts()
+        size = q_set.size
+        probabilities = []
+        for i in counts:
+            probabilities.append(i / size)
+        return probabilities
+        # return list(map(lambda c: c / size, q_set.value_counts()))
+
+
+class dTreeClassifier:
+    def __init__(self):
+        self.tree = None
+        self.labels = []
+        self.dataset = None
+        self.default_class = None
+
+    def fit(self, data, target):
+        combined = pd.DataFrame(data)
+        self.labels = combined.columns
+        combined["class"] = pd.Series(target)
+        self.default_class = combined["class"].value_counts().index[0]
+        self.tree = dTree(combined, self.labels, self.default_class)
+        return self.tree
+
+    def predict(self, data):
+        self.dataset = data
+        predictions = []
+        for index, row in data.iterrows():
+            predictions.append(self.find_one(row))
+        return predictions
+
+    def find_one(self, row):
+        return self.traverse_tree(self.tree.root, row)
+
+    def traverse_tree(self, node, data):
+        if node is None:
+            return self.default_class
+        if node.leaf:
+            return node.label
+        key = int(data[node.label])
+        if key in node.children:
+            return self.traverse_tree(node.children[key], data)
+        return self.default_class
+
--- DecisionTree/main.py
+++ DecisionTree/main.py
@@ -1,11 +1,37 @@
-from DecisionTree.DTreeClassifier import DTreeClassifier
+from DecisionTree.dTree2 import dTreeClassifier
 from common.ClassifierTesterIris import ClassifierTesterIris
+from sklearn.model_selection import train_test_split as tts
+import pandas as pd
+from numpy import NaN
 
 
 def main():
-    classifier = DTreeClassifier()
+    classifier = dTreeClassifier()
     tester = ClassifierTesterIris(classifier)
-    tester.test(100, False, discretize=True)
+    data = load_voting_records()
+    training_data, testing_data, training_targets, testing_targets = tts(
+        data["data"], data["targets"], shuffle=True)
+    classifier.fit(training_data, training_targets)
+    predicted_targets = classifier.predict(testing_data)
+    tester.compare(predicted_targets, testing_targets.values, True)
+
+
+def load_voting_records():
+    columns = [
+        "class", "handicapped", "water", "adoption", "physician", "salvador",
+        "religious", "anti", "aid", "missile", "immigration", "synfuels",
+        "education", "superfund", "crime", "duty", "export"
+    ]
+    voting_data = pd.read_csv(
+        "../datasets/house-votes-84.data.txt", header=None)
+    voting_data.columns = columns
+    voting_data.replace("?", NaN, inplace=True)
+    voting_data.fillna(inplace=True, method="pad")
+    voting_data.replace({"y": 1, "n": 0, "?": -1}, inplace=True)
+    targets = voting_data["class"]
+    voting_data.drop(labels=["class"], axis=1, inplace=True)
+    split = {"data": voting_data, "targets": targets}
+    return split
 
 
 if __name__ == '__main__':
