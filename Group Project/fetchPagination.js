c21d02b55940cf5752d6d27215509461d55c87ca
Bryan Müller <starwarsgeek0975@gmail.com>
2019-03-11 11:51:16 -0600

    get and write course ids

---
+++ Group Project/fetchPagination.js
@@ -0,0 +1,52 @@
+const parseLink = require("parse-link-header");
+const fetch = require("node-fetch");
+
+const devToken = process.env.DEV_TOKEN;
+function fetchPaginatedData(url, data = []) {
+  return new Promise((resolve, reject) =>
+    fetch(url).then(res => {
+      // Reject when fetch error
+      if (res.status !== 200) {
+        console.log(res.status, res.statusText);
+        reject(new Error("Bad fetch"));
+      }
+      // Get pagination link
+      const link = parseLink(res.headers.get("Link"));
+      res.json().then(page => {
+        // Add response data to list
+        data = data.concat(page);
+        // Determine progress
+        const progress = parseFloat(
+          (parseInt(link.current.page) / parseInt(link.last.page)) * 100
+        ).toPrecision(4);
+        printProgress(
+          progress,
+          `Progress: Page ${link.current.page}/${link.last.page}`
+        );
+        // If we are not on the last page, recursively call
+        // fetch on next link
+        if (link.current.page !== link.last.page) {
+          // Recursive call. On resolution/rejected, pass it
+          // up the promise resolution/rejection chain
+          fetchPaginatedData(link.next.url + `&access_token=${devToken}`, data)
+            .then(resolve)
+            .catch(reject);
+        } else {
+          // Otherwise, we're done. Reset stdout
+          // resolve
+          console.log("\n");
+          resolve(data);
+        }
+      });
+    })
+  );
+}
+
+function printProgress(progress, msg) {
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  process.stdout.write(msg + " " + progress + "%");
+}
+module.exports = {
+  fetchPaginatedData
+};
---
+++ Group Project/getCourseIds.js
@@ -0,0 +1,60 @@
+const fs = require("fs").promises;
+const { fetchPaginatedData } = require("./fetchPagination");
+
+const devToken = process.env.DEV_TOKEN;
+
+const url = (path, params = []) => {
+  let url = `https://byui.instructure.com/api/v1${path}?access_token=${devToken}&per_page=50`;
+  if (params) {
+    params.forEach(param => {
+      url += `&${param.key}=${param.value}`;
+    });
+  }
+  return url;
+};
+
+const allCourses = account => `/accounts/${account}/courses`;
+const csAccountId = 67;
+
+/**
+ * Fetches the cs courses and writes the course ids to
+ * a json file
+ */
+function writeCourseIds() {
+  return new Promise((resolve, reject) => {
+    fetchPaginatedData(url(allCourses(csAccountId)))
+      .then(courses => courses.map(course => course.id))
+      .then(courses =>
+        fs.writeFile(
+          "./json/courseIds.json",
+          JSON.stringify({ courses }),
+          "utf8"
+        )
+      )
+      .then(resolve)
+      .catch(e => reject(e));
+  });
+}
+
+/**
+ * returns the course ids of the cs courses.
+ * Checks first for an existing list of courses,
+ * if they don't exist, refetches them
+ */
+function getCourseIds() {
+  return new Promise((resolve, reject) => {
+    try {
+      const ids = require("./json/courseIds.json").courses;
+      resolve(ids);
+    } catch (e) {
+      console.log("Course Ids do not exist... fetching now");
+      writeCourseIds()
+        .then(() => resolve(require("./json/courseIds.json").courses))
+        .catch(reject);
+    }
+  });
+}
+
+module.exports = {
+  getCourseIds
+};
